37f2c56a6b650ad68159e637465707f5f5980b0b
Atikur Rahaman Santo <55808925+ARSSANTO@users.noreply.github.com>
2022-11-11 08:16:56 +0600

    unnecessary tables

--- qbox.sql
+++ qbox.sql
@@ -308,30 +308,6 @@ CREATE TABLE IF NOT EXISTS `bank_cards` (
   KEY `record_id` (`record_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS `gloveboxitems` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `plate` varchar(255) NOT NULL DEFAULT '[]',
-  `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  PRIMARY KEY (`plate`),
-  KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1;
-
-CREATE TABLE IF NOT EXISTS `stashitems` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `stash` varchar(255) NOT NULL DEFAULT '[]',
-  `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  PRIMARY KEY (`stash`),
-  KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1;
-
-CREATE TABLE IF NOT EXISTS `trunkitems` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `plate` varchar(255) NOT NULL DEFAULT '[]',
-  `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  PRIMARY KEY (`plate`),
-  KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1;
-
 CREATE TABLE IF NOT EXISTS `management_funds` (
 `id` INT(11) NOT NULL AUTO_INCREMENT,
 `job_name` VARCHAR(50) NOT NULL,
