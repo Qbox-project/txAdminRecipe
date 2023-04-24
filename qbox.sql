aeb974ce8dc81a4abe68b15eb5069638454bfd56
iLLeniumStudios <104288623+TheiLLeniumStudios@users.noreply.github.com>
2023-04-24 10:02:58 +0200

    feat(sql): Required tables for illenium-appearance

--- qbox.sql
+++ qbox.sql
@@ -218,14 +218,35 @@ CREATE TABLE IF NOT EXISTS `playerskins` (
 CREATE TABLE IF NOT EXISTS `player_outfits` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `citizenid` varchar(50) DEFAULT NULL,
-  `outfitname` varchar(50) NOT NULL,
+  `outfitname` varchar(50) NOT NULL DEFAULT '0',
   `model` varchar(50) DEFAULT NULL,
-  `skin` text DEFAULT NULL,
-  `outfitId` varchar(50) NOT NULL,
+  `props` varchar(1000) DEFAULT NULL,
+  `components` varchar(1500) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `citizenid` (`citizenid`),
-  KEY `outfitId` (`outfitId`)
-) ENGINE=InnoDB AUTO_INCREMENT=1;
+  UNIQUE KEY `citizenid_outfitname_model` (`citizenid`,`outfitname`,`model`),
+  KEY `citizenid` (`citizenid`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `management_outfits` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `job_name` varchar(50) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `minrank` int(11) NOT NULL DEFAULT 0,
+  `name` varchar(50) NOT NULL DEFAULT 'Cool Outfit',
+  `gender` varchar(50) NOT NULL DEFAULT 'male',
+  `model` varchar(50) DEFAULT NULL,
+  `props` varchar(1000) DEFAULT NULL,
+  `components` varchar(1500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `player_outfit_codes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `outfitid` int(11) NOT NULL,
+  `code` varchar(50) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `FK_player_outfit_codes_player_outfits` (`outfitid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS `player_vehicles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
