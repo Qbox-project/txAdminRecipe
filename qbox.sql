b015bab4936be06bdd326e6c31c219d202c96035
ChatDisabled <44729807+ChatDisabled@users.noreply.github.com>
2023-07-02 20:22:27 +0200

    feat(sql): banking tables

--- qbox.sql
+++ qbox.sql
@@ -362,3 +362,33 @@ CREATE TABLE IF NOT EXISTS `ox_doorlock` (
 
 INSERT INTO `ox_doorlock` (`id`, `name`, `data`) VALUES
 	(1, 'vangelico_jewellery', '{"maxDistance":2,"groups":{"police":0},"doors":[{"model":1425919976,"coords":{"x":-631.9553833007813,"y":-236.33326721191407,"z":38.2065315246582},"heading":306},{"model":9467943,"coords":{"x":-630.426513671875,"y":-238.4375457763672,"z":38.2065315246582},"heading":306}],"state":1,"coords":{"x":-631.19091796875,"y":-237.38540649414063,"z":38.2065315246582},"hideUi":true}');
+
+CREATE TABLE IF NOT EXISTS `bank_accounts_new` (
+  `id` varchar(50) NOT NULL,
+  `amount` int(11) DEFAULT 0,
+  `transactions` longtext DEFAULT '[]',
+  `auth` longtext DEFAULT '[]',
+  `isFrozen` int(11) DEFAULT 0,
+  `creator` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+INSERT INTO `bank_accounts_new` (`id`, `amount`, `transactions`, `auth`, `isFrozen`, `creator`) VALUES
+	('ambulance', 0, '[]', '[]', 0, NULL),
+	('cardealer', 0, '[]', '[]', 0, NULL),
+	('mechanic', 0, '[]', '[]', 0, NULL),
+	('police', 0, '[]', '[]', 0, NULL),
+	('realestate', 0, '[]', '[]', 0, NULL),
+	('lostmc', 0, '[]', '[]', 0, NULL),
+	('ballas', 0, '[]', '[]', 0, NULL),
+	('vagos', 0, '[]', '[]', 0, NULL),
+	('cartel', 0, '[]', '[]', 0, NULL),
+	('families', 0, '[]', '[]', 0, NULL),
+	('triads', 0, '[]', '[]', 0, NULL);
+
+CREATE TABLE IF NOT EXISTS `player_transactions` (
+  `id` varchar(50) NOT NULL,
+  `isFrozen` int(11) DEFAULT 0,
+  `transactions` longtext DEFAULT '[]',
+  PRIMARY KEY (`id`)
+);
