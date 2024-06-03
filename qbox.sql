1874d2d34cba0e05ee9d766b0067cf3f8ca5a34e
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2024-06-03 18:40:17 +0200

    feat: swap qbx_apartment for qbx_properties

--- qbox.sql
+++ qbox.sql
@@ -303,3 +303,19 @@ CREATE TABLE IF NOT EXISTS `vehicle_financing` (
     PRIMARY KEY (`vehicleId`),
     FOREIGN KEY `vehicleId` (`vehicleId`) REFERENCES `player_vehicles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 )    ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `properties` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`property_name` VARCHAR(255) NOT NULL, /* simple label that can be used to identify which property it is. I.e. used for spawn list */
+	`coords` JSON NOT NULL, /* vec3 world coords for the entry position */
+	`price` INT NOT NULL DEFAULT 0,
+	`owner` VARCHAR(255), /* citizen ID of the owner */
+	`interior` VARCHAR(255) NOT NULL, /* the interior name, can range from IPL name to a shell hash that needs to spawn in */
+	`keyholders` JSON NOT NULL DEFAULT (JSON_OBJECT()), /* citizen IDs of other people that have access */
+	`rent_interval` INT DEFAULT NULL, /* the rent interval in hours */
+	`interact_options` JSON NOT NULL DEFAULT (JSON_OBJECT()), /* clothing and exit points */
+	`stash_options` JSON NOT NULL DEFAULT (JSON_OBJECT()), /* multiple stash support */
+	`decorations` JSON NOT NULL DEFAULT (JSON_OBJECT()), /* the model name with its corresponding coords that needs to be placed */
+	FOREIGN KEY (owner) REFERENCES `players` (`citizenid`),
+	PRIMARY KEY (id)
+);
