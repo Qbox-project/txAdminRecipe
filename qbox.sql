1966b5c4a6770036613bc4f257958ab1095af978
Shomy <26857315+ShomyTKD@users.noreply.github.com>
2024-09-13 16:26:46 +0200

    fix: add SQL for qbx_weed (#92)

--- qbox.sql
+++ qbox.sql
@@ -306,3 +306,16 @@ CREATE TABLE IF NOT EXISTS `properties` (
 	FOREIGN KEY (owner) REFERENCES `players` (`citizenid`),
 	PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `weed_plants` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `property` varchar(30) NULL,
+  `stage` tinyint NOT NULL DEFAULT 1,
+  `sort` varchar(30) NOT NULL,
+  `gender` enum('male', 'female') NOT NULL,
+  `food` tinyint NOT NULL DEFAULT 100,
+  `health` tinyint NOT NULL DEFAULT 100,
+  `stageProgress` tinyint NOT NULL DEFAULT 0,
+  `coords` tinytext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
