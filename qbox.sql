5d9f34dec888f9c4217a15bfcf78801ab52cd77d
Manason <clanerp@gmail.com>
2024-09-19 08:46:57 -0700

    feat(sql): qbx_management job logging table

--- qbox.sql
+++ qbox.sql
@@ -249,6 +249,19 @@ INSERT INTO `management_funds` (`job_name`, `amount`, `type`) VALUES
 ('families', 0, 'gang'),
 ('triads', 0, 'gang');
 
+CREATE TABLE IF NOT EXISTS `player_jobs_activity` (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `citizenid` VARCHAR(50) COLLATE utf8mb4_unicode_ci,
+  `job` varchar(255) NOT NULL,
+  `last_checkin` int NOT NULL,
+  `last_checkout` int NULL DEFAULT NULL,
+  FOREIGN KEY (`citizenid`) REFERENCES `players` (`citizenid`) ON DELETE CASCADE,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `id` (`id` DESC) USING BTREE,
+  INDEX `last_checkout` (`last_checkout` ASC) USING BTREE,
+  INDEX `citizenid_job` (`citizenid`, `job`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci;
+
 CREATE TABLE IF NOT EXISTS `ox_doorlock` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
