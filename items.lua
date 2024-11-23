06696a5630009f307afc485c611a9c9be68b636b
Matthew <22198949+MafewTM@users.noreply.github.com>
2024-11-22 22:10:19 -0500

    fix(items): conflicts with qbx_medical

--- items.lua
+++ items.lua
@@ -56,12 +56,6 @@ return {
     ['bandage'] = {
         label = 'Bandage',
         weight = 115,
-        client = {
-            anim = { dict = 'missheistdockssetup1clipboard@idle_a', clip = 'idle_a', flag = 49 },
-            prop = { model = `prop_rolled_sock_02`, pos = vec3(-0.14, -0.14, -0.08), rot = vec3(-50.0, -50.0, 0.0) },
-            disable = { move = true, car = true, combat = true },
-            usetime = 2500,
-        }
     },
 
     ['burger'] = {
